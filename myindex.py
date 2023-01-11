71b0d46eff0e03ed81b4206d4ae338fc49072a4e
Hildeberto Macêdo <89755872+hildebertomacedo@users.noreply.github.com>
2023-01-11 15:32:21 -0400

    Update myindex.py

--- myindex.py
+++ myindex.py
@@ -38,5 +38,5 @@ def render_page(pathname):
         return taxa_information.layout
 
 if __name__ == '__main__':
-    port = int (os.envioron.get("PORT", 5000))
+    port = int (os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port = port)
